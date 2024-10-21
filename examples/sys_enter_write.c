ecc9a32c9774f45a8885452d21b303b4480165af
Miek Gieben <miek@miek.nl>
2024-10-21 19:19:58 +0200

    more
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- examples/sys_enter_write.c
+++ examples/sys_enter_write.c
@@ -8,6 +8,6 @@ SEC("tp/syscalls/sys_enter_write")
 int handle_tp(void *ctx)
 {
 	int pid = bpf_get_current_pid_tgid() >> 32;
-	bpf_printk("BPF triggered sys_enter_write from PID %d.\n", pid);
+	bpf_printk("hello %d.\n", pid);
 	return 0;
 }
