d47705763549abbd30af95a3069342c9d6f1b1a8
Miek Gieben <miek@miek.nl>
2024-10-24 08:00:40 +0200

    playing with ssa
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
+
+	"golang.org/x/tools/go/loader"
+	"golang.org/x/tools/go/ssa"
+	"golang.org/x/tools/go/ssa/ssautil"
 )
 
 func main() {
@@ -22,4 +27,39 @@ func main() {
 		return true
 	})
 	ast.Print(fset, prog)
+
+	// Create a loader.Config to hold the program configuration
+	var conf loader.Config
+	conf.Fset = fset
+	conf.CreateFromFiles("main", prog)
+
+	// Load the program from the parsed file
+	prog2, err := conf.Load()
+	if err != nil {
+		fmt.Println("Error loading program:", err)
+		return
+	}
+
+	// Create an SSA program from the loaded program
+	ssaprog := ssautil.CreateProgram(prog2, ssa.SanityCheckFunctions)
+
+	// Build the SSA representation for all packages in the program
+	ssaprog.Build()
+
+	// Print the SSA for each function in the "main" package
+	for _, pkg := range ssaprog.AllPackages() {
+		fmt.Printf("%+v\n", pkg)
+		b := &bytes.Buffer{}
+		ssa.WritePackage(b, pkg)
+		fmt.Printf("%s\n", b)
+		for _, member := range pkg.Members {
+			if fn, ok := member.(*ssa.Function); ok {
+				b.Reset()
+				fmt.Printf("Function %s SSA:\n", fn.Name())
+				ssa.WriteFunction(b, fn)
+				fmt.Printf("%s\n", b)
+			}
+		}
+	}
+
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/miekg/ebpf
 
 go 1.23.2
 
-require github.com/cilium/ebpf v0.16.0
+require golang.org/x/tools v0.26.1-0.20241023200604-1f162c6c5be3
 
 require (
-	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 )
